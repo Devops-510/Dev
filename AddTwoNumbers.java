e7865ea1911613c1d9df98b2e1505fe8c73ad864
Devops-510 <cejcse510@gmail.com>
2024-12-11 16:15:42 +0530

    Update AddTwoNumbers.java

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,7 +1,7 @@
 public class AddTwoNumbers {
     public static void main(String[] args) {
         if(args.length<2) {
-            System.out.println("Please provide two numbers as arguments.")
+            System.out.println("Please provide two numbers as arguments.");
             return;
         }
 
